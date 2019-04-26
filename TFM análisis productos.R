a47ecc87a7d00b7012cc43fd8ba966555eac2ab6
osmaac <osmaac@gmail.com>
2019-04-26 12:07:27 +0200

    Add files via upload

--- TFM análisis productos.R
+++ TFM análisis productos.R
@@ -22,6 +22,7 @@ levels(account$frequency)#Traducimos los niveles la variable frequency (frecuenc
 levels(account$frequency)<-c("Monthly","After_trans","Weekly")# After_trans means "After Transaction" (después de cada transacción) 
 which(is.na(account)) #No hay missings
 introduce(account)
+plot_bar(account)
 unique(duplicated(account$account_id, incomparables = FALSE))#No hay duplicates en account_id
 unique(duplicated(account, incomparables = FALSE))#No hay duplicates en el fichero
 account$account_id #account_id no está ordenado
@@ -44,6 +45,7 @@ which(is.na(client)) #No hay missings
 introduce(client)
 unique(duplicated(client$client_id, incomparables = FALSE))#No hay duplicates en client_id
 unique(duplicated(client, incomparables = FALSE))#No hay duplicates en el fichero
+plot_bar(client)
 
 #Feature birth_number includes birthday and sex: the number is in the form YYMMDD for men, the number is in the
 #form YYMM+50DD for women, where YYMMDD is the date of birth
@@ -76,6 +78,7 @@ unique(duplicated(disp$client_id, incomparables = FALSE))#No hay duplicates en c
 unique(duplicated(disp$account_id, incomparables = FALSE))#Sí hay duplicates en account_id porque hay 2 tipos de relación Owner y Disponent
 unique(duplicated(disp, incomparables = FALSE))#No hay duplicates
 disp %>% filter(type == "OWNER")%>%str()#Vemos que cada account_id tiene un owner
+plot_bar(disp)
 
 #Vamos a incorporar los datos de client_id a cada cuenta utilizando los datos del DataFrame disp 
 account<-mutate(account,client_id_owner=NA,client_id_disponent=NA)
@@ -122,6 +125,7 @@ introduce(loan)
 unique(duplicated(loan$loan_id, incomparables = FALSE))#No hay duplicates en loan_id
 unique(duplicated(loan$account_id, incomparables = FALSE))#No hay duplicates en account_id (por lo tanto cada cuenta sólo tiene 1 loan)
 unique(duplicated(loan, incomparables = FALSE))#No hay duplicates en el fichero Loan
+plot_bar(loan)
 
 #Vamos a trabajar sobre las variables del fichero Loans
 
@@ -163,6 +167,8 @@ which(is.na(order)) #No hay missings
 introduce(order)
 unique(duplicated(order$account_id, incomparables = FALSE))#Puede haber varias orders por cada en account_id
 unique(duplicated(order, incomparables = FALSE))#No hay duplicates en el fichero Loan
+plot_bar(order)#En feature k_symbol hay operaciones que no están categorizadas
+table(order$k_symbol)#Hay 1379 observaciones no categorizadas 
 
 #Pasamos la variable amount de Order, de factor a numérica
 order$amount<-as.numeric(as.character(order$amount)) 
@@ -223,6 +229,7 @@ introduce(card)
 unique(duplicated(card$card_id, incomparables = FALSE))#No hay duplicates en card_id
 unique(duplicated(card$disp_id, incomparables = FALSE))#No hay disp_id con más de una card_id
 unique(duplicated(card, incomparables = FALSE))#No hay duplicates en el fichero
+plot_bar(card)
 
 #La variable issued es un factor y queremos que tenga formato de fecha
 card$issued<-as.character(card$issued)
@@ -232,7 +239,7 @@ card$issued<-card$issued+19000000
 card$issued<-as.Date.character(card$issued, "%Y%m%d")
 
 #Incluimos las columnas que vamos a añadir con los datos de card( si la cuenta tiene tarjeta, que tipo de tarjeta y si es de Owner o disponent)
-account2<-account%>%mutate(
+account<-account%>%mutate(
   `owner_card_type`=numeric(dim(account)[1]),
   `disponent_card_type`=numeric(dim(account)[1])
 )
@@ -266,6 +273,7 @@ str(district)
 which(is.na(district)) #No hay missings
 introduce(district)
 unique(duplicated(district, incomparables = FALSE))#No hay duplicates en el fichero
+plot_bar(district)
 
 #Pásamos a númerico las siguientes variables del fichero de district.La variable A10 que es de ratio of urban inhabitants.
 #La variable A12 que es de Unemployment Rate '95.La variable A13 que es de Unemployment Rate '96
@@ -318,12 +326,191 @@ district<-mutate(district,crimes_96_ratio=district$crimes_96/district$num_inhabi
 #Incorporamos las columnas desde district
 account=merge(account,district,by="district_id", all.x=TRUE)
 
+#Actualizamos el diccionario de account
+hm_acc<-hashmap(account$account_id,seq(1:dim(account)[1]))#Para una key= account_id me da la fila del df account en el que está account_id
+
 #Cargamos el fichero de transacciones (trans) y revisamos su estructura y missings y duplicates
 trans <- read.csv2(file="C:/Master Data Science/Master en Data Science/TFM/Transacciones de Banco Checo/trans.csv", header=TRUE)
-head(trans)
+head(trans)#LA feature "bank account" parece que tiene missings 
 class(trans)
 str(trans)
 which(is.na(trans)) #SI hay missings
 introduce(trans)
 unique(duplicated(trans, incomparables = FALSE))#No hay duplicates en el fichero
-str(which(is.na(trans$k_symbol)))
+plot_intro(trans)
+plot_missing(trans)#Hay missings en la feature account que no la vamos a utilizar porque es la cuenta de destino. No hay missings en account_id que es por la que vamos a vincular
+plot_bar(trans)#Hay transacciones no categorizadas en las features "operation" y "k_symbol" 
+
+#Vamos a buscar los no categorizados en las features de trans
+table(trans$k_symbol)# En k_symbol hay 535.314 observaciones no categorizadas
+table(trans$operation)# En operation hay 183.114 observaciones no categorizadas
+
+#Las variables balance y amount aparecen como factores y deberían ser numéricas
+trans$amount<-as.numeric(as.character(trans$amount)) 
+trans$balance<-as.numeric(as.character(trans$balance))
+
+#Revisamos la feature "balance". Se corresponde con el saldo que tiene la cuenta después de la transacción
+min(trans$balance)#Hay saldos negativos. Puede ser un buen indicador para calidad crediticia
+str(trans[trans$amount==trans$balance,])#En algunos casos (4.501) el saldo después de la transacción se corresponde con el importe de la transacción 
+
+#Recodificamos las variables "type", "operation" y "k_symbol":
+
+#Feature "type": Básicamente indica si la transacción es de cargo o de abono
+trans$type<-recode_factor(trans$type,`PRIJEM` = "Type_Credit", `VYBER` = "Type_VYBER", `VYDAJ` = "Type_withdrawal")
+table(trans$type)#La categoría de "type" VYBER no está traducida, pero sólo una minoría de observaciones las que tienen esa categoría (16.666)
+
+#Feature "operation": Indica si la operación es un envío de dinero (Remittance), un ingreso (collection), un ingreso en efectivo (CashCredit),
+#un reintegro en efectivo (WithdrawalCash) o un reintegro con tarjeta (WithdrawalCreditCard)
+levels(trans$operation)<-c("Op_Null","Op_Remittance","Op_Collection","Op_CashCredit","Op_WithdrawalCash","Op_WithdrawalCreditCard") #Cambiamos la denominación de la variable "operation"
+unique(trans[trans$operation=="Op_Remittance","type"])#Todas las Remittances se correponden con  Type_withdrawal de la feature type
+unique(trans[trans$operation=="Op_Collection","type"])#Todas las Collections se correponden con  Type_Credit de la feature type
+unique(trans[trans$operation=="Op_CashCredit","type"])#Todas las Collections se correponden con  Type_Credit de la feature type
+unique(trans[trans$operation=="Op_WithdrawalCash","type"])#Todas las Collections se correponden con Type_withdrawal y Type_VYBE de la feature type
+unique(trans[trans$operation=="Op_WithdrawalCreditCard","type"])#Todas las Collections se correponden con Type_withdrawal de la feature type
+
+#Feature "k_symbol": Indica si la operación es cobro de pensión (Sym_Pension), un pago de seguro (Sym_Insurance),
+#un pago por saldo negativo (Sym_NegBal), un cobro por intereses de un depósito (Sym_IntDep),
+#un pago por un préstamo (Sym_LoanPayment) o un pago de Household o Statement
+levels(trans$k_symbol)<-c("Sym_Null","Sym_Null2","Sym_Pension","Sym_Insurance","Sym_NegBal","Sym_Household","Sym_Statement","Sym_IntDep","Sym_LoanPayment")
+unique(trans[trans$k_symbol=="Sym_Pension","operation"])#Todas se corresponden con la "operation" Collection 
+unique(trans[trans$k_symbol=="Sym_Insurance","operation"])#Todas se corresponden con "Op_Remittance" y "Op_WithdrawalCash"
+unique(trans[trans$k_symbol=="Sym_NegBal","operation"])#Todas se corresponden con "Op_WithdrawalCash"
+unique(trans[trans$k_symbol=="Sym_Household","operation"])#Todas se corresponden con "Op_Remittance" y "Op_WithdrawalCash"
+unique(trans[trans$k_symbol=="Sym_Statement","operation"])#Todas se corresponden con "Op_WithdrawalCash"
+unique(trans[trans$k_symbol=="Sym_IntDep","operation"])#Todas se corresponden con "Op_Null"
+unique(trans[trans$k_symbol=="Sym_LoanPayment","operation"])#Todas se corresponden con "Op_Remittance"
+
+#Para cada account vamos a completar la información con los datos del DataFrame de transacciones (trans)
+#Se incluye el tipo de opertiva que tiene cada cuenta. Vamos a incluir el tipo de transacciones que realiza cada cuenta
+
+account<-account%>%mutate(
+  #Categorías de la feature type
+  `Num_Type_Credit`=numeric(dim(account)[1]),
+  `Num_Type_VYBER`=numeric(dim(account)[1]),
+  `Num_Type_Withdrawal`=numeric(dim(account)[1]),
+  #Categorías de la feature operation
+  `Num_Op_Null`=numeric(dim(account)[1]),
+  `Num_Op_Remittances`=numeric(dim(account)[1]),
+  `Num_Op_Collection`=numeric(dim(account)[1]),
+  `Num_Op_CashCredit`=numeric(dim(account)[1]),
+  `Num_Op_WithdrawalCash`=numeric(dim(account)[1]),
+  `Num_Op_WithdrawalCreditCard`=numeric(dim(account)[1]),
+  #Categorías de la feature k_symbol
+  `Num_Sym_Null`=numeric(dim(account)[1]),
+  `Num_Sym_Null2`=numeric(dim(account)[1]),
+  `Num_Sym_Pension`=numeric(dim(account)[1]),
+  `Num_Sym_Insurance`=numeric(dim(account)[1]),
+  `Num_Sym_NegBal`=numeric(dim(account)[1]),
+  `Num_Sym_Household`=numeric(dim(account)[1]),
+  `Num_Sym_Statement`=numeric(dim(account)[1]),
+  `Num_Sym_IntDep`=numeric(dim(account)[1]),
+  `Num_Sym_LoanPayment`=numeric(dim(account)[1]),
+  `Balance_in_negative`=numeric(dim(account)[1])
+)
+
+for (row_order in seq(1,dim(trans)[1])){
+  row_account<-hm_acc[[trans[row_order,"account_id"]]]
+  #Completamos los atributos de feature type
+  
+  if (trans[row_order,"type"]=="Type_Credit"){
+    account[row_account,"Num_Type_Credit"]=account[row_account,"Num_Type_Credit"]+1
+  }
+  if (trans[row_order,"type"]=="Type_VYBER"){
+    account[row_account,"Num_Type_VYBER"]=account[row_account,"Num_Type_VYBER"]+1
+  }
+  if (trans[row_order,"type"]=="Type_withdrawal"){
+    account[row_account,"Num_Type_Withdrawal"]=account[row_account,"Num_Type_Withdrawal"]+1
+  }
+#Completamos los atributos de feature operation
+  
+  if (trans[row_order,"operation"]=="Op_Null"){
+    account[row_account,"Num_Op_Null"]=account[row_account,"Num_Op_Null"]+1
+  }
+  if (trans[row_order,"operation"]=="Op_Remittance"){
+    account[row_account,"Num_Op_Remittances"]=account[row_account,"Num_Op_Remittances"]+1 
+  }
+  if (trans[row_order,"operation"]=="Op_Collection"){
+    account[row_account,"Num_Op_Collection"]=account[row_account,"Num_Op_Collection"]+1 
+  } 
+  if (trans[row_order,"operation"]=="Op_CashCredit"){
+    account[row_account,"Num_Op_CashCredit"]=account[row_account,"Num_Op_CashCredit"]+1 
+  }
+  if (trans[row_order,"operation"]=="Op_WithdrawalCash"){
+    account[row_account,"Num_Op_WithdrawalCash"]=account[row_account,"Num_Op_WithdrawalCash"]+1 
+  }  
+  if (trans[row_order,"operation"]=="Op_WithdrawalCreditCard"){
+    account[row_account,"Num_Op_WithdrawalCreditCard"]=account[row_account,"Num_Op_WithdrawalCreditCard"]+1 
+  }  
+#Completamos los atributos de tipo k_symbol
+  
+  if (trans[row_order,"k_symbol"]=="Sym_Null"){
+    account[row_account,"Num_Sym_Null"]=account[row_account,"Num_Sym_Null"]+1
+  }
+  if (trans[row_order,"k_symbol"]=="Sym_Null2"){
+    account[row_account,"Num_Sym_Null2"]=account[row_account,"Num_Sym_Null2"]+1 
+  }
+  if (trans[row_order,"k_symbol"]=="Sym_Pension"){
+    account[row_account,"Num_Sym_Pension"]=account[row_account,"Num_Sym_Pension"]+1 
+  } 
+  if (trans[row_order,"k_symbol"]=="Sym_Insurance"){
+    account[row_account,"Num_Sym_Insurance"]=account[row_account,"Num_Sym_Insurance"]+1 
+  }
+  if (trans[row_order,"k_symbol"]=="Sym_NegBal"){
+    account[row_account,"Num_Sym_NegBal"]=account[row_account,"Num_Sym_NegBal"]+1 
+  }  
+  if (trans[row_order,"k_symbol"]=="Sym_Household"){
+    account[row_account,"Num_Sym_Household"]=account[row_account,"Num_Sym_Household"]+1 
+  } 
+  if (trans[row_order,"k_symbol"]=="Sym_Statement"){
+    account[row_account,"Num_Sym_Statement"]=account[row_account,"Num_Sym_Statement"]+1 
+  }
+  if (trans[row_order,"k_symbol"]=="Sym_IntDep"){
+    account[row_account,"Num_Sym_IntDep"]=account[row_account,"Num_Sym_IntDep"]+1 
+  }
+  if (trans[row_order,"k_symbol"]=="Sym_LoanPayment"){
+    account[row_account,"Num_Sym_LoanPayment"]=account[row_account,"Num_Sym_LoanPayment"]+1 
+  }
+  #Para cada transacción verificamos si el saldo ha quedado en negativo y lo incluimos como una feature más 
+  if (trans[row_order,"balance"]<0){
+    account[row_account,"Balance_in_negative"]=account[row_account,"Balance_in_negative"]+1 
+  }
+}
+
+#Ya hemos recpilado la información que consideramos relevante de la información disponible
+#Hemos completado el DataFrame "account"con la información que inicialmente hemos considerado relevante
+#y a continuación vamos a revisar las variables que hemos obtenido
+
+#La variable district, la vamos a considerar un factor
+account2[,"district_id"]<-as.factor(account2$district_id)
+
+#Ahora vamos a trabajar sobre client_id_disponent y loan_id, 
+account<-account%>%mutate(
+  `account_disponent_bin`=numeric(dim(account)[1]),#Vemos si la cuenta tiene autorizado o no
+  `account_loan_bin`=numeric(dim(account)[1]))#Vemos si la cuenta ha contratado préstamo o no
+  
+#Ahora completamos estas nuevas columnas
+for (row_order in seq(1,dim(account)[1])){
+        #Vemos si la cuenta ha contratado préstamo
+            if (is.na(account[row_order,"loan_id"])==TRUE){
+                  account[row_order,"account_loan_bin"]<-0
+                }
+            else (account[row_order,"account_loan_bin"]<-1)
+      #Vemos si la cuenta tiene autorizado
+            if (is.na(account[row_order,"client_id_disponent"])==TRUE){
+                  account[row_order,"account_disponent_bin"]<-0
+               }
+           else (account[row_order,"account_disponent_bin"]<-1)
+      }
+
+#Convertimos en factor estas nuevas variables
+account[,"account_loan_bin"]<-as.factor(account$account_loan_bin)
+account[,"account_disponent_bin"]<-as.factor(account$account_disponent_bin)
+
+#Factorizamos las variables de Sex
+account[,"sex_owner"]<-as.factor(account$sex_owner)
+account[,"sex_disponent"]<-as.factor(account$sex_disponent)
+
+setwd("C:/Master Data Science/Master en Data Science/TFM/Transacciones de Banco Checo")
+write.csv(account, file="DFTenenciaProductos.csv")
+
+
